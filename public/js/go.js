f2ba303bba6dae985869aeabcd4c2fb135864874
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 18:29:51 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -6,12 +6,15 @@ function isAboutBlank() {
 // Function to replace the iframe in the current window with the current page's URL
 function replaceIframeWithCurrentPage() {
   if (!isAboutBlank()) {
-    const iframe = document.createElement("iframe");
-    iframe.src = window.location.href; // Set the iframe source to the current page's URL
+    var loc = window.location.href;
+    var win = window.location.href = "about:blank";
+    let iframe = win.document.createElement("iframe");
+    iframe.src = loc;
+    iframe.style.top = "0px";
+    iframe.style.left = "0px";
     iframe.style.width = "100%";
     iframe.style.height = "100%";
-    document.body.innerHTML = ''; // Remove existing content
-    document.body.appendChild(iframe); // Add the new iframe to the body
+    iframe.style.border = "none";
   }
 }
 
