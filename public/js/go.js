e8a7ca3c242e0b9b2620ad0a7a4aa5c6fc5ae3a4
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 17:24:20 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const popup = open("about:blank", "_blank");
     if (!popup || popup.closed) {
       alert("You're not seeing the site till you enable popups.");
+      window.location.href = "https://clever.com";
     } else {
       const doc = popup.document;
       const iframe = doc.createElement("iframe");
@@ -22,42 +23,5 @@ document.addEventListener("DOMContentLoaded", function() {
       doc.body.appendChild(iframe);
       window.close();
     }
-  } else {
-    window.location.href = "https://clever.com";
   }
 });
-
-function AB() {
-  let inFrame;
-  try {
-    inFrame = window !== top;
-  } catch (e) {
-    inFrame = true;
-  }
-  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    if (window.location.href !== "about:blank") {
-      const popup = open("about:blank", "_blank");
-      if (!popup || popup.closed) {
-        alert("You're not seeing the site till you enable popups.");
-      } else {
-        const doc = popup.document;
-        const iframe = doc.createElement("iframe");
-        const style = iframe.style;
-        const link = doc.createElement("link");
-        const name = "Clever | Portal";
-        const icon = "src/photos/clever-logo.png";
-        doc.title = name;
-        link.rel = "icon";
-        link.href = icon;
-        iframe.src = window.location.href;
-        style.position = "fixed";
-        style.top = style.bottom = style.left = style.right = 0;
-        style.border = style.outline = "none";
-        style.width = style.height = "100%";
-        doc.head.appendChild(link);
-        doc.body.appendChild(iframe);
-        window.close();
-      }
-    }
-  }
-}
