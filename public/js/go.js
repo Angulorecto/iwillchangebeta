0cba8a78d278a6716862f5f3e94ff742d962b73b
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 17:13:16 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -1,41 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
   if (window.location.href !== "about:blank") {
     const popup = open("about:blank", "_blank");
+    if (!popup || popup.closed) {
+      alert("You're not seeing the site till you enable popups.");
+      window.location.href = "https://clever.com";
+    } else {
       const doc = popup.document;
       const iframe = doc.createElement("iframe");
       const style = iframe.style;
-      const link = doc.createElement("link");
-      const name = "Clever | Portal";
-      const icon = "src/photos/clever-logo.png";
-      doc.title = name;
-      link.rel = "icon";
-      link.href = icon;
-      iframe.src = window.location.href;
-      style.position = "fixed";
-      style.top = style.bottom = style.left = style.right = 0;
-      style.border = style.outline = "none";
-      style.width = style.height = "100%";
+	@@ -22,8 +18,10 @@ document.addEventListener("DOMContentLoaded", function() {
       doc.head.appendChild(link);
       doc.body.appendChild(iframe);
       window.close();
-  } else {
-    alert("You're not seeing the site till you enable popups.");
-    window.location.href = "https://clever.com";
-  };
+    }
+  }
 });
 
 function AB() {
   let inFrame;
-
   try {
     inFrame = window !== top;
   } catch (e) {
     inFrame = true;
   }
-
   if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     if (window.location.href !== "about:blank") {
       const popup = open("about:blank", "_blank");
+      window.location.href = "https://clever.com";
+      if (!popup || popup.closed) {
+        alert("You're not seeing the site till you enable popups.");
+      } else {
         const doc = popup.document;
         const iframe = doc.createElement("iframe");
         const style = iframe.style;
@@ -53,9 +47,7 @@ function AB() {
         doc.head.appendChild(link);
         doc.body.appendChild(iframe);
         window.close();
-    } else {
-      alert("You're not seeing the site till you enable popups.");
-      window.location.href = "https://clever.com";
+      }
     }
   }
 }
