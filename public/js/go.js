c134a8879b31d209363e91aaf5cbf034c2078ee8
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 17:37:05 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -1,33 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (window.location.href !== "about:blank") {
-    const popup = open("about:blank", "_blank");
-    if (!popup || popup.closed) {
-      alert("You're not seeing the site till you enable popups.");
-      window.location.href = "https://clever.com";
-    } else {
-      const doc = popup.document;
-      const iframe = doc.createElement("iframe");
-      const style = iframe.style;
-      const link = doc.createElement("link");
-      const name = "Clever | Portal";
-      const icon = "src/photos/clever-logo.png";
-      doc.title = name;
-      link.rel = "icon";
-      link.href = icon;
+let inFrame
 
-      // Save the window location URL before opening the popup
-      const windowUrl = window.location.href;
+try {
+  inFrame = window !== top
+} catch (e) {
+  inFrame = true
+}
 
-      // Use the saved window location URL for the iframe src
-      iframe.src = windowUrl;
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+  const popup = open("about:blank", "_blank")
+  if (!popup || popup.closed) {
+    alert("You're not seeing the site till you enable popups.")
+    window.location.href = "https://clever.com";
+  } else {
+    const doc = popup.document
+    const iframe = doc.createElement("iframe")
+    const style = iframe.style
+    const link = doc.createElement("link")
 
-      style.position = "fixed";
-      style.top = style.bottom = style.left = style.right = 0;
-      style.border = style.outline = "none";
-      style.width = style.height = "100%";
-      doc.head.appendChild(link);
-      doc.body.appendChild(iframe);
-      window.close();
-    }
+    const name = "My Drive - Google Drive"
+    const icon = "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"
+
+    doc.title = name
+    link.rel = "icon"
+    link.href = icon
+
+    iframe.src = location.href
+    style.position = "fixed"
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = "none"
+    style.width = style.height = "100%"
+
+    doc.head.appendChild(link)
+    doc.body.appendChild(iframe)
+
+    const script = doc.createElement("script")
+    script.textContent = `
+      window.onbeforeunload = function (event) {
+        const confirmationMessage = 'Leave Site?';
+        (event || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+      };
+    `
+    doc.head.appendChild(script)
   }
-});
+}
