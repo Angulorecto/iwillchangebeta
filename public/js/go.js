0b069524b223ff6eff2bdffb7687982a2906bdd4
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 18:07:39 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -1,37 +1,33 @@
-let inFrame;
-
+let inFrame
 try {
-  inFrame = window !== top;
+  inFrame = window !== top
 } catch (e) {
-  inFrame = true;
+  inFrame = true
 }
-
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-  const popup = open("about:blank", "_blank");
+  const popup = open("about:blank", "_blank")
   if (!popup || popup.closed) {
-    alert("You're not seeing the site till you enable popups.");
+    alert("You're not seeing the site till you enable popups.")
     window.location.href = "https://clever.com";
   } else {
-    const doc = popup.document;
-    const iframe = doc.createElement("iframe");
-    const style = iframe.style;
-    const link = doc.createElement("link");
+    const doc = popup.document
+    const iframe = doc.createElement("iframe")
+    const style = iframe.style
+    const link = doc.createElement("link")
+    const name = "My Drive - Google Drive"
+    const icon = "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"
+    doc.title = name
+    link.rel = "icon"
+    link.href = icon
+    iframe.src = location.href
+    style.position = "fixed"
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = "none"
+    style.width = style.height = "100%"
+    doc.head.appendChild(link)
+    doc.body.appendChild(iframe)
 
-    const name = "My Drive - Google Drive";
-    const icon = "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png";
-
-    doc.title = name;
-    link.rel = "icon";
-    link.href = icon;
-
-    iframe.src = location.href;
-    style.position = "fixed";
-    style.top = style.bottom = style.left = style.right = 0;
-    style.border = style.outline = "none";
-    style.width = style.height = "100%";
-
-    doc.head.appendChild(link);
-    doc.body.appendChild(iframe);
+    window.close();
 
     const script = doc.createElement("script")
     script.textContent = `
@@ -40,8 +36,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
         (event || window.event).returnValue = confirmationMessage;
         return confirmationMessage;
       };
-      setTimeout(() => { window.location.href = 'https://example.com'; }, 100); // Redirect the original window after a short delay
     `
-    doc.head.appendChild(script);
+    doc.head.appendChild(script)
   }
 }
