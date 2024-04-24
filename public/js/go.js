11714168391a41ccd0470f5a156cb0092abcff5f
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 17:44:58 -0400

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -1,39 +1,37 @@
 let inFrame
 
 try {
-  inFrame = window !== top
+  inFrame = window !== top;
 } catch (e) {
-  inFrame = true
+  inFrame = true;
 }
 
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-  const popup = open("about:blank", "_blank")
+  const popup = open("about:blank", "_blank");
   if (!popup || popup.closed) {
-    alert("You're not seeing the site till you enable popups.")
+    alert("You're not seeing the site till you enable popups.");
     window.location.href = "https://clever.com";
   } else {
-    const doc = popup.document
-    const iframe = doc.createElement("iframe")
-    const style = iframe.style
-    const link = doc.createElement("link")
+    const doc = popup.document;
+    const iframe = doc.createElement("iframe");
+    const style = iframe.style;
+    const link = doc.createElement("link");
 
-    const name = "My Drive - Google Drive"
-    const icon = "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"
+    const name = "My Drive - Google Drive";
+    const icon = "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png";
 
-    window.close();
+    doc.title = name;
+    link.rel = "icon";
+    link.href = icon;
 
-    doc.title = name
-    link.rel = "icon"
-    link.href = icon
+    iframe.src = location.href;
+    style.position = "fixed";
+    style.top = style.bottom = style.left = style.right = 0;
+    style.border = style.outline = "none";
+    style.width = style.height = "100%";
 
-    iframe.src = location.href
-    style.position = "fixed"
-    style.top = style.bottom = style.left = style.right = 0
-    style.border = style.outline = "none"
-    style.width = style.height = "100%"
-
-    doc.head.appendChild(link)
-    doc.body.appendChild(iframe)
+    doc.head.appendChild(link);
+    doc.body.appendChild(iframe);
 
     const script = doc.createElement("script")
     script.textContent = `
@@ -43,6 +41,6 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
         return confirmationMessage;
       };
     `
-    doc.head.appendChild(script)
+    doc.head.appendChild(script);
   }
 }
