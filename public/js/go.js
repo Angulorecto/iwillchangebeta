bbc36ad6ba5adf0c0872036b5f1ac943f51eebb4
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-24 09:05:52 -0400

    Create go.js

---
+++ public/js/go.js
@@ -0,0 +1,63 @@
+function AB() {
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank");
+    if (!popup || popup.closed) {
+alert("Please allow popups and redirects.");
+    } else {
+const doc = popup.document;
+const iframe = doc.createElement("iframe");
+const style = iframe.style;
+const link = doc.createElement("link");
+const name = "Clever | Portal";
+const icon = "src/photos/clever-logo.png";
+doc.title = name;
+link.rel = "icon";
+link.href = icon;
+iframe.src = "src/pages/home.html";
+style.position = "fixed";
+style.top = style.bottom = style.left = style.right = 0;
+style.border = style.outline = "none";
+style.width = style.height = "100%";
+doc.head.appendChild(link);
+doc.body.appendChild(iframe);
+window.close();
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const popup = open("about:blank", "_blank")
+  if (!popup || popup.closed) {
+    alert("Allow popups if you don't want this to get blocked.")
+  } else {
+    const doc = popup.document
+    const iframe = doc.createElement("iframe")
+    const style = iframe.style
+    const link = doc.createElement("link")
+
+    const name = "Clever | Portal";
+    const icon = "src/photos/clever-logo.png";
+
+    doc.title = name;
+    link.rel = "icon";
+    link.href = icon;
+
+    iframe.src = "public/home.html";
+    style.position = "fixed";
+    style.top = style.bottom = style.left = style.right = 0;
+    style.border = style.outline = "none";
+    style.width = style.height = "100%";
+
+    doc.head.appendChild(link);
+    doc.body.appendChild(iframe);
+    window.close();
+  }
+});
